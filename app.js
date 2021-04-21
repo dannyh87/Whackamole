0c2f5e901ff9bc91066513429fa5a6bcd45d3893
Daniel Hickinbottom <dannyh87@hotmail.co.uk>
2021-04-21 19:58:01 +0100

    Initial Commit

---
+++ app.js
@@ -0,0 +1,19 @@
+
+const 
+
+const buttonSelector = () =>{
+    return Math.floor(Math.random() * 16
+}
+
+const i;
+for (i = 0; i < 16; i++){
+
+}
+
+const clickSquare = document.getElementById("timings").innerHTML = "Button Clicked";
+const onclick 
+
+
+// On Start -> select Random Number / div -> change div colour for 1 second -> if div is selected within 1 second then return 100 points which is accumulative
+// -> for a max 30 seconds
+// game ends - alert with total score
