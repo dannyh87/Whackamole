d36fa13a24dc8b0f057f6b135575b048e6a74abf
Daniel Hickinbottom <dannyh87@hotmail.co.uk>
2021-04-24 09:31:19 +0100

    change time

--- app.js
+++ app.js
@@ -1,19 +1,77 @@
+'use strict'
 
-const 
+const sizeOfGrid = 16
 
-const buttonSelector = () =>{
-    return Math.floor(Math.random() * 16
+let score = 0
+
+// returns a number between 1-a
+const randomNumber = (a) =>{
+    return Math.floor(Math.random() * a) + 1
 }
 
-const i;
-for (i = 0; i < 16; i++){
-
+// Update the score element eith the value in score varible.
+const updateScore = () => {
+    const element = document.getElementById('score')
+    element.innerHTML = `Score: ${score}`
 }
 
-const clickSquare = document.getElementById("timings").innerHTML = "Button Clicked";
-const onclick 
+const clickSquare = (number) => {
+    // console.log(`you clicked on box ${number}`)
+    
+    const element = document.getElementById(`${number}`)
+    // console.log('element', element)
+
+    if (element.classList.contains('mole')) {
+        // Hitting a mole
+        element.classList.remove('mole')
+
+        score += 1
+        updateScore()
+        console.log('You gained a point ?!?!')
+    }
+}
 
 
+const update = () => {
+    console.log('update function')
+
+    const number = randomNumber(sizeOfGrid)
+    // console.log('number', number)
+
+    const element = document.getElementById(`${number}`)
+
+    // console.log('element', element)
+
+    element.classList.add('mole')
+}
+
+
+const gameover = () => {
+    console.log('gameover')
+    clearInterval(updateTimer)
+    alert(`Your score was: ${score}!!!`)
+    // @TODO: reset game...
+}
+
+const gameTime = 10000
+// const gameTime = 30000
+
+let updateTimer // time for the update function
+
+const start = () => {
+    // console.log('start running')
+    updateTimer = setInterval(update, 1000);
+    console.log('updateTimer', updateTimer)
+
+    setTimeout(gameover, gameTime)
+    updateScore();
+}
+
+
+
+
+
+// const onclick 
 // On Start -> select Random Number / div -> change div colour for 1 second -> if div is selected within 1 second then return 100 points which is accumulative
 // -> for a max 30 seconds
 // game ends - alert with total score
